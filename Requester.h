c25de6d00aae9dd09eec3da173ab42b952f15aae
Rayyamada310 <ryotaro.yamada310@gmail.com>
2024-07-15 19:59:42 -0700

    Made changes on Requester module

--- Requester.h
+++ Requester.h
@@ -25,11 +25,12 @@ extern int requesterPosition;
 class Requester {
 public:
     char requester_name[21]; // max 20 chars: last name, first name
-    int phone_number[11]; // YYYY-MM-DD
+    int phone_number[11]; //
     char email[25]; // max 24 chars
     char department[13];
 
     Requester();
+    Requester(const char* name, const int* phone, const char* email_addr, const char* dept);
 };
 
 // Initialize the requester file
